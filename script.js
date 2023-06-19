a14c2726803a102d1f2384b23a5c68adefc36d2e
adam <19adamchristie96@gmail.com>
2023-06-19 01:09:25 +0100

    Create a for loop that creates a new div for each book in the myLibrary array and adds inner text inside the div

---
+++ script.js
@@ -0,0 +1,18 @@
+let myLibrary = ['Book1', 'Book2', 'Book3'];
+let body = document.querySelector('body');
+
+function Book(){
+
+};
+
+function addBookToLibrary(){
+
+};
+
+for(eachBook in myLibrary){
+    let newDiv = document.createElement('div');
+    let title = document.createElement('h2');
+    title.innerText = myLibrary[eachBook];
+    newDiv.appendChild(title);
+    body.appendChild(newDiv);
+}
