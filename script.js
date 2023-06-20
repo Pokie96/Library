26160537c74c87a526c190c64f20446fca79f4ae
adam <19adamchristie96@gmail.com>
2023-06-20 20:34:51 +0100

    Fix the function which creates the elements on the page for the book objects so that it only creates the newest object element

--- script.js
+++ script.js
@@ -19,24 +19,23 @@ function addBookToLibrary(book){
 // This is the function which will create the book cards to display
 // the book information from the myLibrary array.
 function createBookElements(){
-    for(eachBook in myLibrary){
-        let newDiv = document.createElement('div');
-        newDiv.className = 'bookCards'
-        let title = document.createElement('h2');
-        let author = document.createElement('p');
-        let pageCount = document.createElement('p');
-        let beenRead = document.createElement('p');
-        title.innerText = `Title: ${myLibrary[eachBook].title}`;
-        author.innerText = `Author: ${myLibrary[eachBook].author}`;
-        pageCount.innerText = `Page Count: ${myLibrary[eachBook].numberOfPages}`;
-        beenRead.innerText = `Been Read: ${myLibrary[eachBook].isRead}`;
-        newDiv.appendChild(title);
-        newDiv.appendChild(author);
-        newDiv.appendChild(pageCount);
-        newDiv.appendChild(beenRead);
-        container.appendChild(newDiv);
+    let newestBook = myLibrary[(myLibrary.length)-1]
+    let newDiv = document.createElement('div');
+    newDiv.className = 'bookCards'
+    let title = document.createElement('h2');
+    let author = document.createElement('p');
+    let pageCount = document.createElement('p');
+    let beenRead = document.createElement('p');
+    title.innerText = `Title: ${newestBook.title}`;
+    author.innerText = `Author: ${newestBook.author}`;
+    pageCount.innerText = `Page Count: ${newestBook.numberOfPages}`;
+    beenRead.innerText = `Been Read: ${newestBook.isRead}`;
+    newDiv.appendChild(title);
+    newDiv.appendChild(author);
+    newDiv.appendChild(pageCount);
+    newDiv.appendChild(beenRead);
+    container.appendChild(newDiv);
     };
-};
 
 // This is the event listener for the 'Add Book' button which opens 
 // the form for the book data.
@@ -54,16 +53,11 @@ document.querySelector('#submitButton').addEventListener('click', () => {
     let title = document.querySelector('#bookTitle').value;
     let author = document.querySelector('#authorName').value;
     let pageCount = document.querySelector('#numberOfPages').value;
-    let checked = document.querySelector('#checkbox').value;
-    let beenRead;
-    if(checked === 'on'){
-        beenRead = true;
-    } else{
-        beenRead = false;
-    }
-    console.log(title, author, pageCount, checked, beenRead)
+    let checked = document.querySelector('#checkbox').checked;
+    
+    console.log(title, author, pageCount, checked)
 
-    let newBook = new Book(author, title, pageCount, beenRead);
+    let newBook = new Book(author, title, pageCount, checked);
     addBookToLibrary(newBook);
     createBookElements();
 })
