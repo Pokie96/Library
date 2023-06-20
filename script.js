d8566104ef14708e08a26cc192773b3ab1ba240f
adam <19adamchristie96@gmail.com>
2023-06-20 18:28:34 +0100

    The "Add Book" button should display the hidden form when it is clicked

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+// Global variables required throughout the program.
 let myLibrary = [];
 let container = document.querySelector('.bookCardContainer');
+let openFormButn = document.querySelector('.headerButton');
+let bookForm = document.querySelector('.form');
 
+
+// The book constructor which will be used to create my book objects.
 function Book(author, title, numberOfPages, isRead){
     this.author = author
     this.title = title
@@ -8,10 +13,13 @@ function Book(author, title, numberOfPages, isRead){
     this.isRead = isRead
 };
 
+// Adds the new book object on to the myLibrary array.
 function addBookToLibrary(book){
     myLibrary.push(book)
 };
 
+// This is the function which will create the book cards to display
+// the book information from the myLibrary array.
 function createBookElements(){
     for(eachBook in myLibrary){
         let newDiv = document.createElement('div');
@@ -32,6 +40,14 @@ function createBookElements(){
     };
 };
 
+// This is the event listener for the 'Add Book' button which opens 
+// the form for the book data.
+openFormButn.addEventListener('click', () => {
+    bookForm.style.visibility = 'visible';
+})
+
+
+
 const bookOne = new Book('Tolkien', 'Lord of the rings', 500, true);
 const bookTwo = new Book('JK Rowling', 'Harry Potter', 670, false);
 const bookThree = new Book('JK Rowling', 'Harry Potter', 670, false);
@@ -45,3 +61,4 @@ addBookToLibrary(bookFour);
 addBookToLibrary(bookFive);
 addBookToLibrary(bookSix);
 createBookElements();
+
